155a59fec8aed67c80c9c4c2efb7d4df7e786ab9
4UR3L10 <aureliomartinezdros@outlook.com>
2019-06-01 23:19:55 -0400

    Fisnish
    // Check for review

--- src/testencryption/Reverser.java
+++ src/testencryption/Reverser.java
@@ -9,21 +9,42 @@ public class Reverser extends Transpose
         super(s);
     }
 
-    public String reverseText(String word) // CAREFULLLL
+    public String reverseText(String word) 
     {
         // Complete this method so that it reverses the original string
+        
+        // Setting the order of the words of the String.
         StringBuffer result = new StringBuffer(word);
-         
+        result.reverse();   
         
-        result.reverse();
+        // Coverting the StringBuffer result to String.        
+        String str = result.toString();
         
-        return result.toString();        
+        // Getting the words out of the Strings.
+        String [] words = str.split("\\s+");
+        
+        // String to save the reversed words.
+        String strCounter = "";
+        
+        // Recurring the array, reversing each word and storing the words in a String.
+        for (int i = 0; i < words.length; i++) 
+        {
+            StringBuffer newResult = new StringBuffer(words[i]);
+            words[i] = newResult.reverse().toString();   
+            strCounter = strCounter + words[i] + " ";
+        }        
+        
+        return strCounter;      
     }
 
     public String decode(String word)
     {
         // Complete this method so that it reverses the reversed string
-        return "testttttttttttttt";
+               
+        StringBuffer result = new StringBuffer(word);
+        result.reverse();        
+    
+        return result.toString();         
     }   
    
 }
